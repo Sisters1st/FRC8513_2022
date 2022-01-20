bbfdc8f432b26061fe51dbeb0598b09b7b24c222
kellyk2022@villawalsh.org <kellyk2022@villawalsh.org>
2022-01-20 17:02:46 -0500

    Basic robot tank drive works, updated REV libraries

--- FRC8513_2022/src/main/java/frc/robot/Robot.java
+++ FRC8513_2022/src/main/java/frc/robot/Robot.java
@@ -6,41 +6,44 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 /**
- * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
- * the code necessary to operate a robot with tank drive.
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the manifest file in the resource
+ * directory.
  */
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
+  private Joystick joystick;
 
-  private static final int leftDeviceID1 = 2; 
-  private static final int leftDeviceID2 = 3; 
-  private static final int rightDeviceID1 = 4; 
-  private static final int rightDeviceID2 = 5;
+  private static final int leftMotorID1 = 2; 
+  private static final int leftMotorID2 = 3; 
+  private static final int rightMotorID1 = 4; 
+  private static final int rightMotorID2 = 5;
   private CANSparkMax m_leftMotor1;
   private CANSparkMax m_leftMotor2;
   private CANSparkMax m_rightMotor1;
   private CANSparkMax m_rightMotor2;
+  private final Timer m_timer = new Timer();
 
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_leftMotor1 = new CANSparkMax(leftDeviceID1, MotorType.kBrushed);
-    m_leftMotor2 = new CANSparkMax(leftDeviceID2, MotorType.kBrushed);
-    m_rightMotor1 = new CANSparkMax(rightDeviceID1, MotorType.kBrushed);
-    m_rightMotor2 = new CANSparkMax(rightDeviceID2, MotorType.kBrushed);
+    m_leftMotor1 = new CANSparkMax(leftMotorID1, MotorType.kBrushed);
+    m_leftMotor2 = new CANSparkMax(leftMotorID2, MotorType.kBrushed);
+    m_rightMotor1 = new CANSparkMax(rightMotorID1, MotorType.kBrushed);
+    m_rightMotor2 = new CANSparkMax(rightMotorID2, MotorType.kBrushed);
 
     m_leftMotor2.follow(m_leftMotor1);
     m_leftMotor1.setInverted(true);
@@ -49,12 +52,36 @@ public class Robot extends TimedRobot {
     m_rightMotor2.setInverted(true);
 
     m_myRobot = new DifferentialDrive(m_leftMotor1, m_rightMotor1);
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    joystick = new Joystick(0);
   }
 
+  /** This function is run once each time the robot enters autonomous mode. */
+  @Override
+  public void autonomousInit() {
+    m_timer.reset();
+    m_timer.start();
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  /** This function is called once each time the robot enters teleoperated mode. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    m_myRobot.tankDrive(joystick.getY(), joystick.getRawAxis(3));
   }
+
+  /** This function is called once each time the robot enters test mode. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
 }
