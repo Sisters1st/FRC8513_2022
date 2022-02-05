687717c202cd782f3b628b7966d79e34fea3d940
Kristen Kelly <kellyk2022@villawalsh.org>
2022-02-05 10:24:41 -0500

    virtual saturday changes

--- FRC8513_2022/src/main/java/frc/Auto.java
+++ FRC8513_2022/src/main/java/frc/Auto.java
@@ -22,44 +22,37 @@ public class Auto {
         thisRobot.straightPID.reset();
         thisRobot.distancePID.reset();
     }
-
-    /** This function is called periodically during autonomous. */
-    public void autoPeriodic() {
+    
+    public void autoPeriodic()
+    {
         double goalMotorSpeed = 100000;
         double motorDelta = 100000;
         double controllerOutput = 100000;
-        switch ((int) thisRobot.Auto) {
-            case 0:
-                // do nothing auto
-                break;
-            case 11: //turn 90 degrees to the right with PID
+    }
+
+    /** This function is called periodically during autonomous. */
+    public void autoActions() {
+        switch ((int) thisRobot.autoAction) {
+            case 1: //turn 90 degrees to the right with PID
                 controllerOutput = thisRobot.turnPID.calculate(thisRobot.currentAngle, thisRobot.autoGoalAngle);
                 goalMotorSpeed = MathUtil.clamp(controllerOutput, -1, 1);
                 thisRobot.m_myRobot.tankDrive(goalMotorSpeed, -goalMotorSpeed);
+                if(Math.abs(thisRobot.autoGoalAngle - thisRobot.currentAngle) < thisRobot.autoTHold) 
+                {
+                  thisRobot.tHoldCounter++;
+                  if(thisRobot.tHoldCounter > thisRobot.tHoldCounterTHold) 
+                  {
+                    thisRobot.autoActionIsDone = true;
+                  }
+                }
+                else
+                {
+                thisRobot.tHoldCounter = 0;
+                }
                 break;
-            case 12: // drive straight with PID
-                controllerOutput = thisRobot.straightPID.calculate(thisRobot.leftEncoderPosition - thisRobot.rightEncoderPosition, 0);
-                motorDelta = MathUtil.clamp(controllerOutput, -.2, .2);
-                goalMotorSpeed = 0.5;
-                thisRobot.m_myRobot.tankDrive(goalMotorSpeed + motorDelta, goalMotorSpeed - motorDelta);
-                break;
-            case 13: // drive straight backward with PID
-                controllerOutput = thisRobot.straightPID.calculate(thisRobot.leftEncoderPosition - thisRobot.rightEncoderPosition, 0);
-                motorDelta = MathUtil.clamp(controllerOutput, -.2, .2);
-                goalMotorSpeed = -0.5;
-                thisRobot.m_myRobot.tankDrive(goalMotorSpeed + motorDelta, goalMotorSpeed - motorDelta);
-                break;
-            case 14: // drive straight to a distance with PID
+            case 2: // drive straight to a distance with PID
                 double distanceControllerOutput = thisRobot.distancePID.calculate(
-                        (thisRobot.leftEncoderPosition + thisRobot.rightEncoderPosition) / 2, 38.216);
-                goalMotorSpeed = MathUtil.clamp(distanceControllerOutput, -.6, .6);
-                controllerOutput = thisRobot.straightPID.calculate(thisRobot.leftEncoderPosition - thisRobot.rightEncoderPosition, 0);
-                motorDelta = MathUtil.clamp(controllerOutput, -.2, .2);
-                thisRobot.m_myRobot.tankDrive(goalMotorSpeed + motorDelta, goalMotorSpeed - motorDelta);
-                break;
-            case 15: // drive straight to a distance with PID
-                distanceControllerOutput = thisRobot.distancePID.calculate((thisRobot.leftEncoderPosition + thisRobot.rightEncoderPosition) / 2,
-                        -38.216);
+                        (thisRobot.leftEncoderPosition + thisRobot.rightEncoderPosition) / 2, thisRobot.autoDriveSetPoint);
                 goalMotorSpeed = MathUtil.clamp(distanceControllerOutput, -.6, .6);
                 controllerOutput = thisRobot.straightPID.calculate(thisRobot.leftEncoderPosition - thisRobot.rightEncoderPosition, 0);
                 motorDelta = MathUtil.clamp(controllerOutput, -.2, .2);
