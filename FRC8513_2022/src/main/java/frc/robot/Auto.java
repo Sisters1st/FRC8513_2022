d9148bc89184179e15a017df31f297a19562aef9
kellyk2022@villawalsh.org <kellyk2022@villawalsh.org>
2022-03-31 20:05:46 -0400

    Update code

--- FRC8513_2022/src/main/java/frc/robot/Auto.java
+++ FRC8513_2022/src/main/java/frc/robot/Auto.java
@@ -339,7 +339,7 @@ public class Auto {
 
                 thisRobot.autoAction = 1;
                 resetSensors();
-                thisRobot.autoGoalAngle = -89;
+                thisRobot.autoGoalAngle = -93;
                 thisRobot.autoStep++;
                 break;
             case 11: // waiting
@@ -359,7 +359,7 @@ public class Auto {
             case 15: // drive backward
                 thisRobot.autoAction = 2;
                 resetSensors();
-                thisRobot.autoGoalDistance = -4.4;
+                thisRobot.autoGoalDistance = -3.8;
                 thisRobot.autoStep++;
                 break;
             case 16:// waiting
